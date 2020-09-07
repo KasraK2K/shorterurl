316b9c15f9515a0317a8251b336965c9dd0b8a5f
Kasra Karami <kasra_k2k@yahoo.com>
2020-09-07 10:54:15 +0430

    now we can select database type

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const { Sequelize, Model, DataTypes, Op } = require("sequelize");
  * @type {*} create sequelize database in file
  * comment this if you wish save data in memory
  */
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "shorterurl-database/database.sqlite",
-  logging: false,
-});
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "shorterurl-database/database.sqlite",
+//   logging: false,
+// });
 
 /**
  * @type {*} create sequelize database in memory
@@ -18,23 +18,40 @@ const sequelize = new Sequelize({
  */
 // const sequelize = new Sequelize("sqlite::memory:", { logging: false });
 
+let sequelize;
+
+// set database type
+const type = (databaseType) => {
+  if (databaseType === "MEMORY") {
+    sequelize = new Sequelize("sqlite::memory:", { logging: false });
+  } else {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: "shorterurl-database/database.sqlite",
+      logging: false,
+    });
+  }
+
+  // init database schema
+  ShortUrl.init(
+    {
+      short_url: DataTypes.STRING,
+      original_url: DataTypes.STRING,
+    },
+    { sequelize, modelName: "urls" }
+  );
+};
+
 const short_id = shortid.generate();
 
 class ShortUrl extends Model {}
-
-ShortUrl.init(
-  {
-    short_url: DataTypes.STRING,
-    original_url: DataTypes.STRING,
-  },
-  { sequelize, modelName: "urls" }
-);
 
 /**
  * @param {*} original this parameter is your original url as string
  * @return {*} string we call that short_url
  */
 const getShorter = async (original) => {
+  if (!sequelize) type();
   await sequelize.sync();
   const url = await ShortUrl.create({
     short_url: short_id,
@@ -49,6 +66,7 @@ const getShortUrl = (original) => getShorter(original); // i will depricate in v
  * @return {*} original url as string
  */
 const getOriginal = async (short_url) => {
+  if (!sequelize) type(); // if sequelize was undefined create that with sqlite database type
   try {
     const url = await ShortUrl.findOne({
       where: {
@@ -73,6 +91,7 @@ const getOriginal = async (short_url) => {
 const getOriginalUrl = (short_url) => getOriginal(short_url); // i will depricate in version 2.0.0
 
 const purge = async (timestamp) => {
+  if (!sequelize) type(); // if sequelize was undefined create that with sqlite database type
   try {
     ShortUrl.destroy({
       where: {
@@ -85,6 +104,14 @@ const purge = async (timestamp) => {
     throw new Error(`error on purge data: ${error.message}`);
   }
 };
+
+(async () => {
+  // await type("MEMORY");
+  const short = await getShorter("https://www.ias.co.ir/long_path");
+  console.log("short url: ", short);
+})();
+
+module.exports.type = type;
 
 module.exports.getShorter = getShorter;
 module.exports.getShortUrl = getShortUrl;
